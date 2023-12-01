b3375907bf16117c699ecedc14ca293cc5383f77
Magda <111302737+magda1231@users.noreply.github.com>
2023-12-01 20:38:41 +0100

    show authenticated page

--- src/app/userpage/page.tsx
+++ src/app/userpage/page.tsx
@@ -1,25 +1,30 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
-import { sign } from "crypto";
-import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
 export default function ProtectedRoute() {
-  const session = useSession();
-  if (!session) {
-    redirect("localhost:3000");
+  const { data: session, status } = useSession();
+  if (status === "authenticated") {
+    return (
+      <div>
+        This is a protected route.
+        <br />
+        You will only see this if you are authenticated.
+        <Button
+          type="submit"
+          onClick={() => signOut({ callbackUrl: "localhost:3000" })}
+        >
+          Sign Out
+        </Button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        You are not authenticated.
+        <br />
+        You will only see this if you are not authenticated.
+      </div>
+    );
   }
-  return (
-    <div>
-      This is a protected route.
-      <br />
-      You will only see this if you are authenticated.
-      <Button
-        type="submit"
-        onClick={() => signOut({ callbackUrl: "localhost:3000" })}
-      >
-        Sign Out
-      </Button>
-    </div>
-  );
 }

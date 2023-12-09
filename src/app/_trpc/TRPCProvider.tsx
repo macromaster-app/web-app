a62fee3673ba8e01bcf003e8237cfc0c2a067dac
BartoszBrodowski <bartolini10058@gmail.com>
2023-12-09 15:12:06 +0100

    refactor: setup .env variable for url, shorten urls

--- src/app/_trpc/TRPCProvider.tsx
+++ src/app/_trpc/TRPCProvider.tsx
@@ -3,26 +3,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { httpBatchLink } from "@trpc/client"
 import { useState } from "react"
-
 import { trpc } from "./client"
 
 export default function TRPCProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({}))
-  const [trpcClient] = useState(() => 
-    trpc.createClient({
-        links: [
-            httpBatchLink({ 
-                url: "http://localhost:3000/api/trpc" 
-            })
-        ],
-    })
-  )
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL
+    const [queryClient] = useState(() => new QueryClient({}))
+    const [trpcClient] = useState(() => 
+        trpc.createClient({
+            links: [
+                httpBatchLink({ 
+                    url: `${apiUrl}/api/trpc` 
+                })
+            ],
+        })
+    )
 
-  return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    </trpc.Provider>
-  )
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    )
 }
--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -1,10 +1,12 @@
 import { httpBatchLink } from '@trpc/client';
 import { appRouter } from '@/server';
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export const serverClient = appRouter.createCaller({
     links: [
         httpBatchLink({
-            url: 'http://localhost:3000/api/trpc',
+            url: `${apiUrl}/api/trpc`,
         }),
     ]
 })

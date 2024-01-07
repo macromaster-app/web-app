6054d670325aa5959b971dfcfd6c0bd02575aeda
mlukjaniuk <ma.lukjaniuk@gmail.com>
2024-01-07 17:20:37 +0100

    fix: fixed not working module

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,14 @@
 import { prisma } from './../prisma';
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<Response> {
   const users = await prisma.users.findMany();
-  return {
-    body: users,
-  };
+
+  const responseBody = JSON.stringify(users);
+  const body = new TextEncoder().encode(responseBody);
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }

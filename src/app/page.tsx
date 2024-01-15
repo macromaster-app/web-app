0f21d500a64876e6c5a86a751ebb285da50dbfe1
Bartosz Brodowski <bartolini10058@gmail.com>
2024-01-15 11:03:58 +0100

    fix(register): delete lastName and change firstName to username

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import MealList from '@/components/MealList'
+import MealList from '@/components/MealList';
 
 export default function Home() {
-  return (
-    <main className="flex-1 flex-col items-center justify-center gap-6">
-      <MealList />
-    </main>
-  )
+	return (
+		<main className='flex-1 flex-col items-center justify-center gap-6'>
+			<MealList />
+		</main>
+	);
 }

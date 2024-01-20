6feabb11c0b8ef39d33d0e9f593c9049445031f0
Magda <111302737+magda1231@users.noreply.github.com>
2024-01-20 22:05:49 +0100

    change visibility for user logged not with credentials

--- src/app/(userpages)/settings/layout.tsx
+++ src/app/(userpages)/settings/layout.tsx
@@ -1,18 +1,11 @@
 // import { Metadata } from "next";
 "use client";
 import Image from "next/image";
-import { getServerSession } from "next-auth";
-// import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
 import { Separator } from "@/components/ui/separator";
 
 import { SidebarNav } from "@/components/Settings/sidebar-nav";
 import { useSession } from "next-auth/react";
-
-// export const metadata: Metadata = {
-//   title: "Forms",
-//   description: "Advanced form example using react-hook-form and Zod.",
-// };
 
 const sidebarNavItems = [
   {
@@ -43,10 +36,8 @@ interface SettingsLayoutProps {
 // };
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
-  // const session = getServerSession(authOptions);
-  // sessionData();
   const session = useSession();
-  // const provider = session.data?.user.provider;
+  const provider = session.data?.user.provider;
   return (
     <>
       <div className=" space-y-6 p-10 pb-16">
@@ -57,12 +48,11 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
           <aside className="-mx-4 lg:w-1/5 flex flex-col">
-            {true ? (
+            {provider == "credentials" ? (
               <SidebarNav items={sidebarNavItems} />
             ) : (
               <SidebarNav items={[sidebarNavItems[0], sidebarNavItems[3]]} />
             )}
-            {/* <SidebarNav items={sidebarNavItems} /> */}
           </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
         </div>

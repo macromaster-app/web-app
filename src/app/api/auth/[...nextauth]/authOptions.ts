3278d4da65b53d70830a3152e839afcf254e4f6d
Magda <111302737+magda1231@users.noreply.github.com>
2024-01-20 21:47:45 +0100

    refactor(auth): added provider parameter to session

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,23 @@
 import GoogleProvider from "next-auth/providers/google";
+import { DefaultSession, Session } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { JWT } from "next-auth/jwt";
+import { Account } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      provider?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    provider?: string;
+  }
+}
 
 export const authOptions = {
   providers: [
@@ -10,5 +29,28 @@ export const authOptions = {
   pages: { signIn: "/signin" },
   session: {
     maxAge: 60 * 30,
+  },
+  callbacks: {
+    async jwt({ token, account }: { token: JWT; account: Account }) {
+      if (account) {
+        token.provider = account.provider;
+        return token;
+      }
+      return token;
+    },
+
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: Session;
+      token: JWT;
+      user: any;
+    }) {
+      session.user.provider = token.provider;
+
+      return session;
+    },
   },
 };
